0965584fac3cddd0aa2b10a20f38c342f52e86a3
joecollier <joseph.collier@dm-companies.com>
2017-12-16 06:12:17 -0500

    Removing debug code from Spotify (provider) Kahlan test

--- spec/Test/Oauth2/Client/Provider/SpotifySpec.php
+++ spec/Test/Oauth2/Client/Provider/SpotifySpec.php
@@ -6,7 +6,6 @@ describe(Spotify::class, function () {
         beforeEach(function () {
             $config = [];
             $this->spotify_provider = new \Datand\Spotify\Oauth2\Client\Provider\Spotify($config);
-            // $this->access_token = new \League\OAuth2\Client\Token\AccessToken([]);
         });
 
         it('sets public URIs', function () {
@@ -17,10 +16,6 @@ describe(Spotify::class, function () {
             expect($this->spotify_provider->getBaseAccessTokenUrl([]))->toEqual(
                 'https://accounts.spotify.com/api/token'
             );
-
-            // expect($this->spotify_provider->getResourceOwnerDetailsUrl(null))->toEqual(
-            //     'https://accounts.spotify.com/authorize'
-            // );
         });
     });
 });
